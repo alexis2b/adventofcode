e6f4b4946b69cd405bb2082dc29bc73d8b857aac
Alexis de Bernis <alexis@bernis.org>
2015-12-06 17:57:13 +0100

    Day 3 solutions

---
+++ day03/House.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace day03
+{
+    internal sealed class House : IEquatable<House>
+    {
+        private readonly int _x;
+        private readonly int _y;
+
+        public House(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public int X { get { return _x; } }
+        public int Y { get { return _y; } }
+
+        public bool Equals(House other)
+        {
+            return other != null && other._x == _x && other._y == _y;
+        }
+
+        public override bool  Equals(object obj)
+        {
+            return Equals(obj as House);
+        }
+
+        public override int GetHashCode()
+        {
+            return _x.GetHashCode() ^ _y.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return String.Format("House({0},{1})", _x, _y);
+        }
+    }
+}
---
+++ day03/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+namespace day03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentHouse = new House(0, 0);
+            var houses       = new HashSet<House>( new[] { currentHouse } );
+
+            string orders;
+            using (var inputReader = new StreamReader("input.txt"))
+                orders = inputReader.ReadToEnd();
+
+            // Part 1
+            foreach (var order in orders)
+            {
+                currentHouse = GetNextHouse(currentHouse, order);
+                houses.Add(currentHouse);
+            }
+            Console.WriteLine("Part 1 - Number of houses visited: " + houses.Count);
+
+
+            // Part 2
+            var currentHouses = new House[] { new House(0, 0), new House(0, 0) };  // Santa, Robo-Santa
+            houses = new HashSet<House>( currentHouses );
+            var player = 0;
+            foreach (var order in orders)
+            {
+                currentHouses[player] = GetNextHouse(currentHouses[player], order);
+                houses.Add(currentHouses[player]);
+                player = 1 - player;
+            }
+            Console.WriteLine("Part 2 - Number of houses visited: " + houses.Count);
+
+            Console.ReadKey();
+        }
+
+        private static House GetNextHouse(House currentHouse, char order)
+        {
+            switch (order)
+            {
+                case '>': return new House(currentHouse.X + 1, currentHouse.Y);
+                case '<': return new House(currentHouse.X - 1, currentHouse.Y);
+                case '^': return new House(currentHouse.X, currentHouse.Y + 1);
+                case 'v': return new House(currentHouse.X, currentHouse.Y - 1);
+                default: return currentHouse;
+            }
+        }
+    }
+}
