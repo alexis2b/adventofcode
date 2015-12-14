4cd04ae51acaa27296c2877fd82fb40c019fba49
Alexis de Bernis <alexis@bernis.org>
2015-12-14 21:06:20 +0100

    Day 13 solution

---
+++ day13/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace day13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var seatingRelationships = File.ReadAllLines("input.txt").Select(SeatingRelationship.FromString).ToArray();
+            var guests               = seatingRelationships.SelectMany( c => new[] {c.Person1, c.Person2} ).Distinct().ToArray();
+
+            int maxHappinessChange = int.MinValue;
+            FindMaxHappinessChange(seatingRelationships, null, null, guests, 0, ref maxHappinessChange);
+            Console.WriteLine("Part 1 - Solution: " + maxHappinessChange);
+
+            // Part 2 - add myself
+            const string me = "Alexis";
+            var newSeatingRelationships = seatingRelationships.Union(guests.SelectMany(g => new[] { 
+                new SeatingRelationship( me, g, 0 ),
+                new SeatingRelationship( g, me, 0 ) })).ToArray();
+            var newGuests = guests.Union(new[] { me }).ToArray();
+            int maxHappinessChange2 = int.MinValue;
+            FindMaxHappinessChange(newSeatingRelationships, null, null, newGuests, 0, ref maxHappinessChange2);
+            Console.WriteLine("Part 2 - Solution: " + maxHappinessChange2);
+
+            Console.ReadKey();
+        }
+
+        private static void FindMaxHappinessChange(SeatingRelationship[] seatingRelationships, string firstGuest, string currentGuest, string[] remainingGuests, int currentHappinessChange, ref int maxHappinessChange)
+        {
+            if ( remainingGuests.Length == 0 )
+            {
+                currentHappinessChange += HappinessChange(seatingRelationships, currentGuest, firstGuest);
+                if (currentHappinessChange > maxHappinessChange)
+                    maxHappinessChange = currentHappinessChange;
+            }
+            else
+                foreach (var nextGuest in remainingGuests)
+                {
+                    var happinessChange = String.IsNullOrEmpty(currentGuest) ? 0 : HappinessChange(seatingRelationships, currentGuest, nextGuest);
+                    FindMaxHappinessChange(seatingRelationships, firstGuest ?? nextGuest, nextGuest, remainingGuests.Where(g => g != nextGuest).ToArray(), currentHappinessChange + happinessChange, ref maxHappinessChange);
+                }
+        }
+
+        private static int HappinessChange(SeatingRelationship[] seatingRelationships, string person1, string person2)
+        {
+            return seatingRelationships.Sum(p => p.Match(person1, person2) ? p.HappinessChange : 0);
+        }
+    }
+}
---
+++ day13/SeatingRelationship.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace day13
+{
+    internal sealed class SeatingRelationship
+    {
+        private static readonly Regex SeatingRelationshipEx = new Regex(@"(?<person1>\w+) would (?<gainOrLose>gain|lose) (?<units>\d+) happiness units by sitting next to (?<person2>\w+)\.", RegexOptions.Compiled);
+        private readonly string _person1;
+        private readonly string _person2;
+        private readonly int    _happinessChange;
+
+        public SeatingRelationship(string person1, string person2, int happinessChange)
+        {
+            _person1         = person1;
+            _person2         = person2;
+            _happinessChange = happinessChange;
+        }
+
+        public string Person1         { get { return _person1;         } }
+        public string Person2         { get { return _person2;         } }
+        public int    HappinessChange { get { return _happinessChange; } }
+
+        public static SeatingRelationship FromString(string seatingRelationshipString)
+        {
+            var match = SeatingRelationshipEx.Match( seatingRelationshipString );
+            Debug.Assert(match.Success);
+
+            return new SeatingRelationship(
+                match.Groups["person1"].Value,
+                match.Groups["person2"].Value,
+                ( match.Groups["gainOrLose"].Value == "gain" ? 1 : -1 ) * int.Parse( match.Groups["units"].Value ) );
+
+        }
+
+        public bool Match(string aPerson, string anotherPerson)
+        {
+            return (aPerson == _person1 || aPerson == _person2) && (anotherPerson == _person2 || anotherPerson == _person1);
+        }
+    }
+}
