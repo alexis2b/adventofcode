e9244869b372030770504f21454f0cd48ecba747
Alexis de Bernis <alexis@bernis.org>
2015-12-14 21:44:22 +0100

    Day 14 solution

---
+++ day14/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace day14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reindeers = File.ReadAllLines("input.txt").Select(Reindeer.FromString).ToArray();
+            Console.WriteLine("Part 1 - Solution: " + reindeers.Select(r => r.GetDistanceAfter(2503)).Max());
+
+            // Part 2
+            var points = new Dictionary<string, int>(reindeers.Length);
+            reindeers.ToList().ForEach(r => points[r.Name] = 0);
+
+            for (var t = 1; t <= 2503; t++)
+            {
+                var maxDistance = reindeers.Select(r => r.GetDistanceAfter(t)).Max();
+                reindeers.Where(r => r.GetDistanceAfter(t) == maxDistance).ToList().ForEach(r => points[r.Name]++);
+            }
+            Console.WriteLine("Part 2 - Solution: " + points.Values.Max());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ day14/Reindeer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+
+namespace day14
+{
+    internal sealed class Reindeer
+    {
+        private static readonly Regex ReindeerEx = new Regex(@"(?<name>\w+) can fly (?<speed>\d+) km/s for (?<flying>\d+) seconds, but then must rest for (?<resting>\d+) seconds\.", RegexOptions.Compiled);
+
+        private readonly string _name;
+        private readonly int    _speed;
+        private readonly int    _flyingTime;
+        private readonly int    _restingTime;
+
+        private Reindeer(string name, int speed, int flyingTime, int restingTime)
+        {
+            _name        = name;
+            _speed       = speed;
+            _flyingTime  = flyingTime;
+            _restingTime = restingTime;
+        }
+
+        public static Reindeer FromString(string reindeerString)
+        {
+            var match = ReindeerEx.Match(reindeerString);
+            Debug.Assert(match.Success);
+
+            return new Reindeer(match.Groups["name"].Value, int.Parse(match.Groups["speed"].Value), int.Parse(match.Groups["flying"].Value), int.Parse(match.Groups["resting"].Value));
+        }
+
+        public string Name { get { return _name; } }
+
+        public int GetDistanceAfter(int seconds)
+        {
+           return
+                (seconds / (_flyingTime + _restingTime)) * _flyingTime * _speed  // full segments
+                + Math.Min(seconds % (_flyingTime + _restingTime), _flyingTime) * _speed; // last segment remainder
+        }
+    }
+}
