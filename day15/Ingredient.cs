55420018f3fdb2cef0df7aee4af680c4e0e43e25
Alexis de Bernis <alexis@bernis.org>
2015-12-15 06:22:51 +0100

    Day 15 solution

---
+++ day15/Ingredient.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+
+namespace day15
+{
+    internal sealed class Ingredient
+    {
+        private static readonly Regex IngredientEx = new Regex(@"(?<name>\w+): capacity (?<capacity>-?\d+), durability (?<durability>-?\d+), flavor (?<flavor>-?\d+), texture (?<texture>-?\d+), calories (?<calories>-?\d+)", RegexOptions.Compiled); 
+
+        private readonly string _name;
+        private readonly int    _capacity;
+        private readonly int    _durability;
+        private readonly int    _flavor;
+        private readonly int    _texture;
+        private readonly int    _calories;
+
+        public Ingredient(string name, int capacity, int durability, int flavor, int texture, int calories)
+        {
+            _name = name;
+            _capacity = capacity;
+            _durability = durability;
+            _flavor = flavor;
+            _texture = texture;
+            _calories = calories;
+        }
+
+        public int Capacity { get { return _capacity; } }
+        public int Durability { get { return _durability; } }
+        public int Flavor { get { return _flavor; } }
+        public int Texture { get { return _texture; } }
+        public int Calories { get { return _calories; } }
+
+        public static Ingredient FromString(string ingredientString)
+        {
+            var match = IngredientEx.Match(ingredientString);
+            Debug.Assert(match.Success);
+
+            return new Ingredient(
+                match.Groups["name"].Value,
+                int.Parse(match.Groups["capacity"].Value),
+                int.Parse(match.Groups["durability"].Value),
+                int.Parse(match.Groups["flavor"].Value),
+                int.Parse(match.Groups["texture"].Value),
+                int.Parse(match.Groups["calories"].Value)
+                );
+        }
+    }
+}
---
+++ day15/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace day15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ingredients = File.ReadAllLines("input.txt").Select( Ingredient.FromString ).ToArray();
+
+            var quantities = new int[ingredients.Length];
+            var best = 0;
+            var bestLight = 0;
+            while( true )
+            {
+                for(int i = 0; i < ingredients.Length-1; i++ )
+                {
+                     quantities[i]++;
+                    if ( quantities[i] > 100 )
+                        quantities[i] = 0;
+                    else
+                        break;
+                }
+
+                var quantityApplied = quantities.Take(  ingredients.Length-1 ).Sum();
+                if ( quantityApplied == 0 )
+                    break;
+                if ( quantityApplied > 100 )
+                    continue;
+
+                quantities[ ingredients.Length-1 ] = 100 - quantityApplied;
+
+                // compute the result
+                var capacity = 0;
+                var durability = 0;
+                var flavor = 0;
+                var texture = 0;
+                var calories = 0;
+                for(int i = 0; i < ingredients.Length; i++)
+                {
+                    capacity += quantities[i] * ingredients[i].Capacity;
+                    durability += quantities[i] * ingredients[i].Durability;
+                    flavor += quantities[i] * ingredients[i].Flavor;
+                    texture += quantities[i] * ingredients[i].Texture;
+                    calories += quantities[i] * ingredients[i].Calories;
+                }
+
+                var total = Math.Max(0, capacity) * Math.Max(0, durability) * Math.Max(0, flavor) * Math.Max(0, texture);
+                if ( total > best )
+                    best = total;
+                if (calories == 500 && total > bestLight)
+                    bestLight = total;
+            }
+            Console.WriteLine("Part 1 - Solution: " + best);
+            Console.WriteLine("Part 2 - Solution: " + bestLight);
+
+            Console.ReadKey();
+        }
+    }
+}
